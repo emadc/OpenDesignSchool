9cd577c8c77d312ec598c2872cf3346025994714
edicarlo <edicarlo@NAVIDEV003.devel>
2018-04-26 16:19:36 +0200

    css home

--- db_config.php
+++ db_config.php
@@ -2,7 +2,7 @@
 ; 	  Le connecteur SQL 	  ;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 [sql];
-ods[host] = localhost;
-ods[user] = "root";
-ods[pass] = "";
-ods[base] = devinette;
+ods[host] = mariadb.devel;
+ods[user] = "nav_int_fai";
+ods[pass] = "7x6H5mQNGhPXARyq";
+ods[base] = nav_intranet_fai;
