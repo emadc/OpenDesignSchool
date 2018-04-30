85b456deb0e1ead598bc902ca2bccf4f62a54e63
auxilia <auxilia@localhost>
2018-04-30 11:17:47 +0200

    validation contact

--- controller/Contact.php
+++ controller/Contact.php
@@ -1,67 +1,80 @@
 <?php
 
-
 /**
  * Class Contact
  *
  * use to show the contact page
  */
-class Contact
-{
-
-	public function showContact($params)
-	{
+class Contact {
+	public function showContact($params) {
+		$manager = new LayoutManager ();
 		
-		$manager = new LayoutManager();
+		$menu = $manager->getMenu ();
 		
-		$menu = $manager->getMenu();
+		$footer = $manager->getFooter ();
 		
-		$footer = $manager->getFooter();
+		$valid = $params['valid'] ? $params['valid'] : null;
 		
-		$myView = new View('contact');
-		$myView->render(array('menu' => $menu, 'footer' => $footer));
+		$myView = new View ( 'contact' );
+		$myView->render ( array (
+				'menu' => $menu,
+				'footer' => $footer,
+				'valid' => $valid
+		) );
 	}
-
-    public function edit($params)
-    {
-        extract($params);
-
-        if(isset($id)) {
-
-            $manager = new ContactManager();
-            $contact = $manager->find($id);
-
-        } else {
-            $contact = new ContactObj();
-        }
-
-        $myView = new View('edit');
-        $myView->render(array('contact' => $contact));
-
-    }
-    
-    public function save($params)
-    {
-//     	echo "params<pre>"; var_dump($params); 
-        extract($params);
-//         echo "values<pre>"; var_dump($values); exit();
-        $manager = new ContactManager();
-        $manager->save($values);
-
-        $myView = new View();
-        $myView->redirect('contact');
-        
-    }
-
-    public function delDev($params)
-    {
-        extract($params);
-        $manager = new OpsManager();
-        $manager->delete($id);
-
-        $myView = new View();
-        $myView->redirect('contact');
-    }
-
-
+	
+	/**
+	 *
+	 * @param unknown $params        	
+	 */
+	public function edit($params) {
+		extract ( $params );
+		
+		if (isset ( $id )) {
+			
+			$manager = new ContactManager ();
+			$contact = $manager->find ( $id );
+		} else {
+			$contact = new ContactObj ();
+		}
+		
+		$myView = new View ( 'edit' );
+		$myView->render ( array (
+				'contact' => $contact 
+		) );
+	}
+	
+	/**
+	 *
+	 * @param unknown $params        	
+	 */
+	public function save($params) {
+		extract ( $params );
+		echo "values<pre>";
+		var_dump ( $values );
+		
+		while ( $value = current ( $values ) ) {
+			$valuesClean [key ( $values )] = $this->test_input ( $value );
+			next ( $values );
+		}
+// 		echo "values<pre>"; var_dump ( $valuesClean ); exit ();
+		$manager = new ContactManager ();
+		$manager->save ( $valuesClean );
+		
+		$myView = new View ();
+		$myView->redirect ( 'contact/valid/1' );
+	}
+	
+	/**
+	 *
+	 * @param unknown $data        	
+	 * @return string
+	 */
+	public function test_input($data) {
+		$data = trim ( $data );
+		$data = stripslashes ( $data );
+		$data = filter_var($data, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+		$data = filter_var($data, FILTER_SANITIZE_STRING);
+		return $data;
+	}
 }
--- view/contact.php
+++ view/contact.php
@@ -1,7 +1,15 @@
 				<section id="contact" class="col-12 ">
 					<div class="form_container">
+						<?php if ($valid):?>
+							<div class="alert alert-primary alert-dismissible fade show" role="alert">
+							  <strong>Holy guacamole!</strong> You should check in on some of those fields below.
+							  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+							    <span aria-hidden="true">&times;</span>
+							  </button>
+							</div>
+						<?php endif;?>
 						<h1 class="h1_title">Contact</h1>
-						<form class="row no-gutters" action="<?php echo HOST;?>contact-save" method="post">
+						<form class="row no-gutters" action="<?php echo htmlspecialchars(HOST.'contact-save');?>" method="post">
 							<div class="form-group col-md-4 col-12">
 								<input type="text" class="form-control" name="values[nom_prenom]" aria-describedby="emailHelp" placeholder="Nom & prénom" autocomplete="off" required>
 								<input type="email" class="form-control" name="values[email]" aria-describedby="emailHelp" placeholder="Email" autocomplete="off" required>
