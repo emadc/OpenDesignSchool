db6a0b173853f334c6cc1324d580b2657572d929
auxilia <auxilia@localhost>
2018-05-06 10:27:35 +0200

    Open Graph

--- view/_head.php
+++ view/_head.php
@@ -5,8 +5,12 @@
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<meta name="description" content="">
-	  	<meta name="author" content="">
+		<meta name="description" content="Your favorite buisiness site multi purpose template">
+	  	<meta name="author" content="Emanuele Di Carlo">
+	  	<meta property="og:title" content="Open Design School templates" />
+		<meta property="og:type" content="website" />
+		<meta property="og:url" content="www.opendesignschool.com" />
+		<meta property="og:image" content="<?php echo HOST;?>/assets/img/logo.png" />
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
 		<link href="https://fonts.googleapis.com/css?family=Raleway:400,700" rel="stylesheet">
 		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
